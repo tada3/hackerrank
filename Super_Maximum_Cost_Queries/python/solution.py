2894677265009d6d3fc00921034eeac73ef1aaf3
Tada Yoshihara <tada3@outlook.com>
2023-05-07 00:01:28 +0900

    renamed

--- Super_Maximum_Cost_Queries/python/solution.py
+++ Super_Maximum_Cost_Queries/python/solution.py
@@ -6,136 +6,100 @@ import random
 import re
 import sys
 import bisect
+from operator import itemgetter
+from itertools import groupby
 
+def build_path_counts(t):
+	t1 = sorted(t, key=itemgetter(2))
 
+	roots = [0] * (len(t1) + 2)
+	sizes = {}
+	path_counts = {}
+	count = 0
+	key_idx = []
+	for k, g in groupby(t1, key=itemgetter(2)):
+		for u, v, _ in g:
+			count += add_path(u, v, roots, sizes)
+		path_counts[k] = count	
+		key_idx.append(k)
 
-def build_cost_rank(t):
-	cost_tab = {}
-	cost_rank = {}
-	
-	for u, v, w in t:
-		#print('uvw', u, v, w)
-		if u not in cost_tab:
-			if v not in cost_tab:
-				# case 1: both new nodes
-				add_new_new(u, v, w, cost_tab, cost_rank)
-			else:
-				# case 2: new node + existing node
-				add_new_existing(u, v, w, cost_tab, cost_rank)
+	return key_idx, path_counts
+
+def add_path(u, v, rs, ss):
+	added = 0
+	if rs[u] == 0:
+		if rs[v] == 0:
+			# case 1: both new nodes
+			added = add_new_new(u, v, rs, ss)
 		else:
-			if v not in cost_tab:
-				# case 2 new node + existing node
-				add_new_existing(v, u, w, cost_tab, cost_rank)
-			else:
-				# case 3: both existing nodes
-				add_existing_existing2(u, v, w, cost_tab, cost_rank)
-		#print('TTT', u, v,cost_tab)
-		
-	#print('tab', cost_tab)
-	#print('rank', cost_rank)
-	return cost_rank
+			# case 2: new node + existing node
+			added = add_new_existing(u, v, rs, ss)
+	else:
+		if rs[v] == 0:
+			# case 2 new node + existing node
+			added = add_new_existing(v, u, rs, ss)
+		else:
+			# case 3: both existing nodes
+			added = add_existing_existing2(u, v, rs, ss)
+	return added
+
+
+
+def get_root(x, rs):
+	current = x
+	while True:
+		#print('LOOP', current)
+		r = rs[current]
+		if r == current:
+			return r
+		current = r	
+
+def get_size(x, rs, ss):
+	r = get_root(x, rs)
+	return ss[r]	
+
+
+
+def add_new_new(u, v, rs, ss):
+	if u > v:
+		u, v = v, u
+	rs[u] = u
+	rs[v] = u
+	ss[u] = 2
+	return 1
+
+
+def add_new_existing(u, v, rs, ss):
+	nodes_in_v = get_size(v, rs, ss)
+	new_leader = get_root(v, rs)
+	if u < new_leader:
+		rs[new_leader] = u
+		new_leader = u
+	rs[u] = new_leader
+	
+	ss[new_leader] = nodes_in_v + 1	
+	return nodes_in_v
 	
 
-def add_new_new(u, v, w, tab, rank):
-	#print('add_new_new 000', u, v, w)
-	tab[u] = {v: w}
-	tab[v] = {u: w}
-	rank[w] = rank.get(w, 0) + 1
-
-def add_new_existing(u, v, w, tab, rank):
-	#print('add_new_existing 000', u, v, w)
-	tab[u] = {}
-	for x, c in tab[v].items():
-		c1 = max(c, w)
-		tab[u][x] = c1
-		tab[x][u] = c1
-		rank[c1] = rank.get(c1, 0 ) + 1
-
-	tab[u][v] = w
-	tab[v][u] = w
-	rank[w] = rank.get(w, 0) + 1
-	
-
-def add_existing_existing2(u, v, w, tab, rank):
-	#xkeys = list(tab[u].keys())
-	#ykeys = list(tab[v].keys())
-
+def add_existing_existing2(u, v, rs, ss):
 	# path(x, y)
-	for x, cx in tab[u].items():
-		for y, cy in tab[v].items():
-			c1 = max(cx, w, cy)
-			tab[x][y] = c1
-			tab[y][x] = c1
-			rank[c1] = rank.get(c1, 0) + 1
+	nodes_in_u = get_size(u, rs, ss)
+	nodes_in_v = get_size(v, rs, ss)
 
-	# path(x, v)
-	tab_v = {}
-	for x, cx in tab[u].items():
-		c1 = max(cx, w)
-		tab[x][v] = c1
-		#tab[v][x] = c1
-		tab_v[x] = c1
-		rank[c1] = rank.get(c1, 0) + 1
-	
-	# path(u, y)
-	tab_u = {}
-	for y, cy in tab[v].items():
-		# path(u, y)
-		c1 = max(w, cy)
-		#tab[u][y] = c1
-		tab_u[y] = c1
-		tab[y][u] = c1
-		rank[c1] = rank.get(c1, 0) + 1
-	
-	#for x in xkeys:
-	#	tab[v][x] = tab[x][v]
-	tab[v].update(tab_v)
-	tab[u].update(tab_u)
+	lu = get_root(u, rs)
+	#lv = get_root(v, rs)
+	#new_leader = lv
+	new_leader = get_root(v, rs)
 
-	#for y in ykeys:
-	#	tab[u][y] = tab[y][u]
+	if lu < new_leader:
+		rs[new_leader] = lu
+		new_leader = lu
+	else:
+		rs[lu] = new_leader
 
-	tab[u][v] = w
-	tab[v][u] = w
-	rank[w] = rank.get(w, 0) + 1
+	ss[new_leader] = nodes_in_u + nodes_in_v
+	return nodes_in_u * nodes_in_v
 
-
-def add_existing_existing(u, v, w, tab, rank):
-	xkeys = list(tab[u].keys())
-	ykeys = list(tab[v].keys())
-
-	# path(x, y)
-	for x, cx in tab[u].items():
-		for y, cy in tab[v].items():
-			c1 = max(cx, w, cy)
-			tab[x][y] = c1
-			tab[y][x] = c1
-			rank[c1] = rank.get(c1, 0) + 1
-
-	# path(x, v)
-	for x, cx in tab[u].items():
-		c1 = max(cx, w)
-		tab[x][v] = c1
-		#tab[v][x] = c1
-		rank[c1] = rank.get(c1, 0) + 1
-	
-	# path(u, y)
-	for y, cy in tab[v].items():
-		# path(u, y)
-		c1 = max(w, cy)
-		#tab[u][y] = c1
-		tab[y][u] = c1
-		rank[c1] = rank.get(c1, 0) + 1
-	
-	for x in xkeys:
-		tab[v][x] = tab[x][v]
-
-	for y in ykeys:
-		tab[u][y] = tab[y][u]
-
-	tab[u][v] = w
-	tab[v][u] = w
-	rank[w] = rank.get(w, 0) + 1
 
 #
 # Complete the 'solve' function below.
@@ -148,14 +112,11 @@ def add_existing_existing(u, v, w, tab, rank):
 
 def solve(tree, queries):
     # Write your code here
-	# 1. Build cost rank
-	# cost_rank[c]: number of paths of cost c
-	cost_rank = build_cost_rank(tree)
-
-	# 2. Sort it
-	key_idx = sorted(cost_rank.keys())
+	# 1. Build path_counts
+	# path_counts[c]: number of paths of cost c
+	key_idx, path_counts = build_path_counts(tree)
 		
-	# 3. Process queries 
+	# 2. Process queries 
 	Q = len(queries)
 	result = [0] * Q
 	for i in range(Q):
@@ -164,11 +125,10 @@ def solve(tree, queries):
 		right = bisect.bisect_left(key_idx, r, lo=left)
 		if right >= len(key_idx) or key_idx[right] > r:
 			right -= 1
-		count = 0
-		for j in range(left, right + 1):
-			idx = key_idx[j]
-			count += cost_rank[key_idx[j]]
-		result[i] = count
+		left -= 1
+		left_val = path_counts[key_idx[left]] if left >= 0 else 0
+		right_val = path_counts[key_idx[right]]
+		result[i] = right_val - left_val
 	return result
 
 
---
+++ Super_Maximum_Cost_Queries/python/solution_slow.py
@@ -0,0 +1,199 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+import bisect
+
+
+
+def build_cost_rank(t):
+	cost_tab = {}
+	cost_rank = {}
+	
+	for u, v, w in t:
+		#print('uvw', u, v, w)
+		if u not in cost_tab:
+			if v not in cost_tab:
+				# case 1: both new nodes
+				add_new_new(u, v, w, cost_tab, cost_rank)
+			else:
+				# case 2: new node + existing node
+				add_new_existing(u, v, w, cost_tab, cost_rank)
+		else:
+			if v not in cost_tab:
+				# case 2 new node + existing node
+				add_new_existing(v, u, w, cost_tab, cost_rank)
+			else:
+				# case 3: both existing nodes
+				add_existing_existing2(u, v, w, cost_tab, cost_rank)
+		#print('TTT', u, v,cost_tab)
+		
+	#print('tab', cost_tab)
+	#print('rank', cost_rank)
+	return cost_rank
+	
+
+def add_new_new(u, v, w, tab, rank):
+	#print('add_new_new 000', u, v, w)
+	tab[u] = {v: w}
+	tab[v] = {u: w}
+	rank[w] = rank.get(w, 0) + 1
+
+def add_new_existing(u, v, w, tab, rank):
+	#print('add_new_existing 000', u, v, w)
+	tab[u] = {}
+	for x, c in tab[v].items():
+		c1 = max(c, w)
+		tab[u][x] = c1
+		tab[x][u] = c1
+		rank[c1] = rank.get(c1, 0 ) + 1
+
+	tab[u][v] = w
+	tab[v][u] = w
+	rank[w] = rank.get(w, 0) + 1
+	
+
+def add_existing_existing2(u, v, w, tab, rank):
+	#xkeys = list(tab[u].keys())
+	#ykeys = list(tab[v].keys())
+
+	# path(x, y)
+	for x, cx in tab[u].items():
+		for y, cy in tab[v].items():
+			c1 = max(cx, w, cy)
+			tab[x][y] = c1
+			tab[y][x] = c1
+			rank[c1] = rank.get(c1, 0) + 1
+
+	# path(x, v)
+	tab_v = {}
+	for x, cx in tab[u].items():
+		c1 = max(cx, w)
+		tab[x][v] = c1
+		#tab[v][x] = c1
+		tab_v[x] = c1
+		rank[c1] = rank.get(c1, 0) + 1
+	
+	# path(u, y)
+	tab_u = {}
+	for y, cy in tab[v].items():
+		# path(u, y)
+		c1 = max(w, cy)
+		#tab[u][y] = c1
+		tab_u[y] = c1
+		tab[y][u] = c1
+		rank[c1] = rank.get(c1, 0) + 1
+	
+	#for x in xkeys:
+	#	tab[v][x] = tab[x][v]
+	tab[v].update(tab_v)
+	tab[u].update(tab_u)
+
+	#for y in ykeys:
+	#	tab[u][y] = tab[y][u]
+
+	tab[u][v] = w
+	tab[v][u] = w
+	rank[w] = rank.get(w, 0) + 1
+
+
+def add_existing_existing(u, v, w, tab, rank):
+	xkeys = list(tab[u].keys())
+	ykeys = list(tab[v].keys())
+
+	# path(x, y)
+	for x, cx in tab[u].items():
+		for y, cy in tab[v].items():
+			c1 = max(cx, w, cy)
+			tab[x][y] = c1
+			tab[y][x] = c1
+			rank[c1] = rank.get(c1, 0) + 1
+
+	# path(x, v)
+	for x, cx in tab[u].items():
+		c1 = max(cx, w)
+		tab[x][v] = c1
+		#tab[v][x] = c1
+		rank[c1] = rank.get(c1, 0) + 1
+	
+	# path(u, y)
+	for y, cy in tab[v].items():
+		# path(u, y)
+		c1 = max(w, cy)
+		#tab[u][y] = c1
+		tab[y][u] = c1
+		rank[c1] = rank.get(c1, 0) + 1
+	
+	for x in xkeys:
+		tab[v][x] = tab[x][v]
+
+	for y in ykeys:
+		tab[u][y] = tab[y][u]
+
+	tab[u][v] = w
+	tab[v][u] = w
+	rank[w] = rank.get(w, 0) + 1
+
+#
+# Complete the 'solve' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. 2D_INTEGER_ARRAY tree
+#  2. 2D_INTEGER_ARRAY queries
+#
+
+def solve(tree, queries):
+    # Write your code here
+	# 1. Build cost rank
+	# cost_rank[c]: number of paths of cost c
+	cost_rank = build_cost_rank(tree)
+
+	# 2. Sort it
+	key_idx = sorted(cost_rank.keys())
+		
+	# 3. Process queries 
+	Q = len(queries)
+	result = [0] * Q
+	for i in range(Q):
+		l, r = queries[i]
+		left = bisect.bisect_left(key_idx, l)
+		right = bisect.bisect_left(key_idx, r, lo=left)
+		if right >= len(key_idx) or key_idx[right] > r:
+			right -= 1
+		count = 0
+		for j in range(left, right + 1):
+			idx = key_idx[j]
+			count += cost_rank[key_idx[j]]
+		result[i] = count
+	return result
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    q = int(first_multiple_input[1])
+
+    tree = []
+
+    for _ in range(n - 1):
+        tree.append(list(map(int, input().rstrip().split())))
+
+    queries = []
+
+    for _ in range(q):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    result = solve(tree, queries)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
