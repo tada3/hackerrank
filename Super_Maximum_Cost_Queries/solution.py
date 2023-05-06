c355d4716474ce50a2671688b18e6098e4285dd8
Tada Yoshihara <tada3@outlook.com>
2023-05-06 16:14:44 +0900

    First solution for Super Maximum Cost Queries

---
+++ Super_Maximum_Cost_Queries/solution.py
@@ -0,0 +1,104 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+import bisect
+
+#
+# Complete the 'solve' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. 2D_INTEGER_ARRAY tree
+#  2. 2D_INTEGER_ARRAY queries
+#
+
+def solve(tree, queries):
+    # Write your code here
+	N = len(tree) + 1
+	# 1. Build cost table 
+	# cost_tab[U][V]: cost of the path {U, V}
+	#cost_tab = [ [0] * NP for i in range(NP) ]
+	cost_tab = {}
+	cost_rank = {}
+	#processed = [False] * NP
+
+	u, v, w = tree[0]
+	cost_tab[u] = {v: w}
+	cost_tab[v] = {u: w}
+	cost_rank[w] = 1
+	for i in range(1, N-1):
+		u, v, w = tree[i]
+		print(u, v, w)
+		if u in cost_tab:
+			# Make sure u is new node
+			u, v = v, u
+		cost_tab[u] = {v: w}
+		cost_rank[w] = cost_rank.get(w, 0) + 1
+		for x, c in cost_tab[v].items():
+			c1 = max(c, w)
+			cost_tab[u][x] = c1
+			cost_tab[x][u] = c1
+			cost_rank[c1] = cost_rank.get(c1, 0 ) + 1
+		cost_tab[v][u] = w
+
+	print('cost_tab', cost_tab)
+	print('cost_rank', cost_rank)
+
+
+	# 2. Sort it
+	key_idx = sorted(cost_rank.keys())
+	print('key_idx', key_idx)
+	
+		
+	# 3. Process queries 
+	Q = len(queries)
+	result = [0] * Q
+	for i in range(Q):
+		l, r = queries[i]
+		print("l,r", l, r)
+		left = bisect.bisect_left(key_idx, l)
+#		if key_idx[left] > l:
+#			left += 1
+		right = bisect.bisect_left(key_idx, r, lo=left)
+		if right >= len(key_idx) or key_idx[right] > r:
+			right -= 1
+		count = 0
+		print("left, right", left, right)
+		for j in range(left, right + 1):
+			print("j", j)
+			idx = key_idx[j]
+			print(idx)
+			count += cost_rank[key_idx[j]]
+		result[i] = count
+	return result
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    q = int(first_multiple_input[1])
+
+    tree = []
+
+    for _ in range(n - 1):
+        tree.append(list(map(int, input().rstrip().split())))
+
+    queries = []
+
+    for _ in range(q):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    result = solve(tree, queries)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
